8232f3d8dde1b50195a98a4863caa1b2d72c18cd
mpdissanayake <165764981+mpdissanayake@users.noreply.github.com>
2024-12-01 13:18:57 +0530

    export studentRouterand inport it in index.js file clear the code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import Student from "./models/student.js";
+import studentRouter from "./routes/studentRouter.js";
 
 const app =express();
 const mongoUrl="mongodb+srv://admin:123@cluster0.pka0s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -18,37 +19,6 @@ connection.once("open",()=>{
 })
 
 app.use (bodyParser.json())
-
-app.get("/",
-  (req,res)=>{
-
-    console.log()
-
-    console.log(req.body)
-    console.log("This is get reqest");
-  })
-
-app.post("/",
-  (req,res)=>{
-
-   
-    const newStudent =new Student(req.body)
-
-    newStudent.save().then(()=>{
-
-    res.json({
-      messege:"Student saved successfully"
-    });
-})
-.catch((error)=>{
-        res.json({
-          message:"Error saving student",
-          error:error.message
-        })
-      }
-
-    )
-  })
 
 app.listen(3000,()=>{
     console.log("Suver is running on port 3000")
