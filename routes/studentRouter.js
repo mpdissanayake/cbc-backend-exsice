774bf840fc7d5ad1c43bd588c170065056e368a4
mpdissanayake <165764981+mpdissanayake@users.noreply.github.com>
2024-12-01 21:24:32 +0530

    clear code for save student in db

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -4,17 +4,11 @@ import Student from "../models/student.js";
 const studentRouter=express.Router();//create studentRouter//
 
 studentRouter.get("/",(req,res)=>{
-    console.log("This is get request from student router")
-    res.json({
-        message:"this is get request for student router"
-    })
+    
 })
 
 studentRouter.post("/",(req,res)=>{
-    console.log("This is post request from student router")
-    res.json({
-        message:"this is post request from student Router"
-    })
+    
 
 })
 
