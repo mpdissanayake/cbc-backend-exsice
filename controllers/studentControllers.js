9c0cb657cbdd0abcd79f8be19a5c364ebb8ef8e9
mpdissanayake <165764981+mpdissanayake@users.noreply.github.com>
2024-12-02 12:14:47 +0530

    controllers folde and file created

---
+++ controllers/studentControllers.js
@@ -0,0 +1,24 @@
+import studentRouter from "../routes/studentRouter";
+//create student router unction//
+studentRouter.get("/",(req,res)=>{
+    (studentList)=>{
+        res.json({
+            list : studentList
+        })
+    }
+})
+
+studentRouter.post("/",(req,res)=>{
+
+    const student=new Student(req.body)
+    student.save().then(()=>{
+        res.json({
+            message : "Student Created "
+        })
+    }).catch(()=>{
+        res.json({
+            message : " Student is no created"
+        })
+    })
+})
+export default studentRouter;
